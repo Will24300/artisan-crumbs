ff2452c5bb0d5ac3492edbc9b4d77b157e5d6d33
Volonte Rwicha <volonterwicha2004@gmail.com>
2026-06-10 00:42:28 +0200

    feat: create navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,29 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
+import Layout from "./components/Layout";
+import ErrorPage from "./pages/Error";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="contact" element={<Contact />} />
+
+      <Route path="*" element={<ErrorPage />} />
+    </Route>,
+  ),
+);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import { Search, ShoppingCart, User } from "lucide-react";
+import iconImg from "../assets/Icon.png";
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-5 px-15 border-b border-gray-200">
+      <div className="flex justify-between items-center gap-10">
+        <div className="flex items-center gap-1">
+          <img src={iconImg} alt="bread icon" />
+          <h2 className="font-bold text-xl">Artisan Crumbs</h2>
+        </div>
+        <ul className="flex justify-between items-center gap-5 text-[#334155] text-[15px]">
+          <li className="cursor-pointer">Shop</li>
+          <li className="cursor-pointer">Daily Specials</li>
+          <li className="cursor-pointer">Our Story</li>
+          <li className="cursor-pointer">Contact</li>
+        </ul>
+      </div>
+      <div className="flex justify-between items-center gap-5">
+        <div className="flex justify-between items-center gap-2 bg-[#F1F5F9] py-2 px-5 rounded-[25px] text-[#94A3B8]">
+          <Search size={17} />
+          <input
+            type="text"
+            placeholder="Search pastries..."
+            className="bg-transparent outline-none text-[14px]"
+          />
+        </div>
+        <div className="bg-[#F1F5F9] p-2 rounded-3xl cursor-pointer">
+          <ShoppingCart size={17} />
+        </div>
+        <div className="bg-[#F1F5F9] p-2 rounded-3xl cursor-pointer">
+          <User size={17} />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
