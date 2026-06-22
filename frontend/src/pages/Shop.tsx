69a26edbb8936fdf24ce7ac65a9bd9483f5f541e
Volonte Rwicha <volonterwicha2004@gmail.com>
2026-06-22 18:08:31 +0200

    feat: add shop links

--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom";
-
 function Shop() {
   return (
     <section>
-      <Link to="/">Back</Link>
+      <div>
+        <h1>
+          Our Bakery <span>Fresh</span> Daily
+        </h1>
+        <div>
+          <p>
+            Handcrafted with organic flour and local ingredients. Experience the
+            art of traditional baking.
+          </p>
+          <ul>
+            <li>All items</li>
+            <li>Cake</li>
+            <li>Muffins</li>
+            <li>Croissant</li>
+            <li>Bread</li>
+            <li>Tart</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
